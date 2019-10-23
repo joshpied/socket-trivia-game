52da7baae670a36a748551c6312fe71af7e4283a
Joshua Piedimonte <josh@Joshuas-MacBook-Pro-2.local>
2019-10-23 13:49:25 -0400

    Finished

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// I, Joshua Piedimonte, student number 000746786, certify that all code submitted is my own work; that I have not copied it from any other source, I also certify that I have not allowed my work to be copied by others.
 const app = require('express')();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
